6dad3637e844f0757b869b7a167c10de98f71582
Maxim Novikov <maxnovbel@mail.ru>
2020-10-12 23:41:58 +0300

    feat: finish landing, no adaptive after section.land, no dropdown on click

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,68 @@
 window.addEventListener("DOMContentLoaded", function() {
+
+  function slider(wrapper){
+    $(wrapper).slick({
+      infinite: false,
+      speed: 300,
+      slidesToShow: 4,
+      variableWidth: true,
+      prevArrow: `
+        <button 
+          class="slick-prev section__arrow" 
+            type="button">
+          <svg width="17" 
+            height="30" 
+            viewBox="0 0 17 30" 
+            fill="none">
+              <path d="M16 1L2 15L16 29" 
+                stroke-width="2"/>
+          </svg>
+        </button>`,
+      nextArrow: `
+        <button 
+          class="slick-next section__arrow" 
+          type="button">
+            <svg width="17" 
+              height="30" 
+              viewBox="0 0 17 30" 
+              fill="none">
+              <path d="M1 1L15 15L1 29" 
+                stroke-width="2"/>
+            </svg>
+        </button>`
+    });
+  }
   
-  $('.dinner__slider').slick({
-    infinite: false,
-    speed: 300,
-    slidesToShow: 4,
-    variableWidth: true,
-    prevArrow: '<button class="slick-prev section__arrow" type="button"><svg width="17" height="30" viewBox="0 0 17 30" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16 1L2 15L16 29" stroke-width="2"/></svg></button>',
-    nextArrow: '<button class="slick-next section__arrow" type="button"><svg width="17" height="30" viewBox="0 0 17 30" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1L15 15L1 29" stroke-width="2"/></svg></button>'
-  });
+  slider('.dinner__slider');
+  slider('.main-menu__slider');
 
-  $('.main-menu__slider').slick({
+  $('.reviews__slider').slick({
     infinite: false,
-    speed: 300,
-    slidesToShow: 4,
-    variableWidth: true,
-    prevArrow: '<button class="slick-prev section__arrow" type="button"><svg width="17" height="30" viewBox="0 0 17 30" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M16 1L2 15L16 29" stroke-width="2"/></svg></button>',
-    nextArrow: '<button class="slick-next section__arrow" type="button"><svg width="17" height="30" viewBox="0 0 17 30" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 1L15 15L1 29" stroke-width="2"/></svg></button>'
+    arrows: true,
+    prevArrow: `
+      <button 
+        class="slick-prev reviews__arrow" 
+        type="button">
+          <svg width="10" 
+                height="16" 
+                viewBox="0 0 10 16" 
+                fill="none">
+            <path d="M9 1L2 8L9 15"
+              stroke-width="2"/>
+          </svg>
+      </button>`,
+    nextArrow: `
+      <button 
+        class="slick-next reviews__arrow" 
+        type="button">
+          <svg width="10" 
+                height="16" 
+                viewBox="0 0 10 16" 
+                fill="none" >
+            <path d="M1 1L8 8L1 15" 
+              stroke-width="2"/>
+          </svg>
+      </button>`
   });
-
 
 });
